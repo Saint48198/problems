3c54c652be414299f382f1163d089879d2485bb6
sdaniels <daniels.scott.r@gmail.com>
2015-09-09 20:10:21 -0400

    fixed issue with getting prime values from a entry of a very large number.

--- largestPrime.js
+++ largestPrime.js
@@ -5,39 +5,77 @@
     "use strict";
     function largestPrime (num) {
         this.num = num;
-        this.calculate = function () {
-            var max = this.num - 1;
-            var largestPrime = this.num;
-            while(max) {
-                if (this.num % max === 0 && max > 1 && this.primeCheck(max)) {
-                    largestPrime = max;
-                    break;
-                }
-                max--;
-            }
+        this.primeFactors = [];
+        this.initialize = function () {
 
-            return largestPrime;
+            if (this.isPrime(num)) {
+                this.primeFactors.push(num);
+            } else {
+                this.findPrimeNumbers(this.num);
+            }
         }
-        this.primeCheck = function (num) {
-            var max = num - 1;
-            var isPrime = true;
-            for (var i = 2; i < max; i++) {
-                if (num % i === 0) {
-                    isPrime = false;
+
+        this.findPrimeNumbers = function(num) {
+            var newMax = 0;
+
+            for (var i = 2; i < num; i++) {
+                if (num % i === 0 && this.inArray(i) === false) {
+                    newMax = num / i;
+                    this.primeFactors.push(i);
+
+                    // using recursion to find the next prime number as long as the new max is not a prime number itself
+                    if (this.isPrime(newMax)) {
+                        this.primeFactors.push(newMax);
+                    } else {
+                        this.findPrimeNumbers(newMax);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        this.inArray = function (value) {
+            var found = false;
+            for(var i = 0, total = this.primeFactors.length; i < total; i++) {
+                if (value === this.primeFactors[i]) {
+                    found = true;
                     break;
                 }
             }
 
-            return isPrime
+            return found;
+        }
+
+        this.isPrime = function (value) {
+            for(var i = 2, total = value - 1; i < total; i++) {
+                if (value % i === 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        this.getValue = function () {
+            return this.primeFactors[this.primeFactors.length - 1];
+        }
+
+        this.printValue = function(value) {
+            console.log(value);
         }
     }
 
-    var test1 = new largestPrime(13195);
-    console.log(test1.calculate());
+    // test cases
+    var test1 = new largestPrime(600851475143); // 6857
+    test1.initialize();
+    test1.printValue(test1.getValue());
 
-    var test2 = new largestPrime(11);
-    console.log(test2.calculate());
+    var test2 = new largestPrime(11); // 11
+    test2.initialize();
+    test2.printValue(test1.getValue());
 
-    var test3 = new largestPrime(600851475143);
-    console.log(test3.calculate());
+    var test3 = new largestPrime(13195); // 29
+    test3.initialize();
+    test3.printValue(test1.getValue());
+
 })();
