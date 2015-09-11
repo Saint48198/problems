0e820284562dbfa8afa2f39bb60da6d8ffd953b7
sdanielspq <scott.daniels@proquest.com>
2015-09-11 13:20:44 -0400

    Added initial question to the comments with link to where is was found for the first 4 questions.

--- largestPrime.js
+++ largestPrime.js
@@ -1,5 +1,8 @@
 /**
  * Created by sdaniel1 on 9/9/15.
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ * https://projecteuler.net/problem=3
  */
 (function () {
     "use strict";
